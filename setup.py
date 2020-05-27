48639a43b7949aaa2b2edf00918b4767e77933de
mtoshi <mtoshi@outlook.com>
2020-05-27 20:12:24 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""setup."""
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+requires = []
+with open('requirements.txt', 'w') as _file:
+    _file.write('\n'.join(requires))
+
+setup(
+    name='pynacl-demo',
+    version='0.0.1',
+    url='https://github.com/mtoshi/pynacl-demo',
+    author='mtoshi',
+    author_email='mtoshi@outlook.com',
+    description='Experimental code.',
+    packages=find_packages(),
+    install_requires=requires,
+)
